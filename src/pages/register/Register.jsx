138efa52d1fa5c62796bf0a2353532c41bf32cff
KI-Anik <iamkhairul101@gmail.com>
2025-02-13 19:08:27 +0600

    add authentication context and Firebase integration; implement user registration with Lottie animation

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,26 +1,44 @@
+import Lottie from "lottie-react";
+import registerLottieData from '../../assets/lottie/Register-Animation.json'
+import { useContext } from "react";
+import AuthContext from "../../context/authContext/AuthContext";
 
 const Register = () => {
+    const {createUser} = useContext(AuthContext)
+
+    const handleSubmit = e=>{
+        e.preventDefault()
+        const form = e.target;
+        const email = form.email.value
+        const password = form.password.value
+        console.log(email, password)
+
+        createUser(email,password)
+        .then(result=>{
+            console.log(result.user)
+        })
+        .catch(err=>{
+            console.log(err.message)
+        })
+    }
     return (
         <div className="hero bg-base-200 min-h-screen">
             <div className="hero-content flex-col lg:flex-row-reverse">
-                <div className="text-center lg:text-left">
-                    <p className="py-6">
-                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                        quasi. In deleniti eaque aut repudiandae et a id nisi.
-                    </p>
+                <div className="text-center lg:text-left w-2/5">
+                   <Lottie animationData={registerLottieData}></Lottie>
                 </div>
                 <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
                 <h1 className="ml-8 mt-4 text-5xl font-bold">Register now!</h1>
 
                     <div className="card-body">
-                        <fieldset className="fieldset">
+                        <form onSubmit={handleSubmit} className="fieldset">
                             <label className="fieldset-label">Email</label>
-                            <input type="email" className="input" placeholder="Email" />
+                            <input type="email" className="input" name="email" placeholder="Email" />
                             <label className="fieldset-label">Password</label>
-                            <input type="password" className="input" placeholder="Password" />
+                            <input type="password" className="input" name="password" placeholder="Password" />
                             <div><a className="link link-hover">Forgot password?</a></div>
                             <button className="btn btn-neutral mt-4">Register</button>
-                        </fieldset>
+                        </form>
                     </div>
                 </div>
             </div>
