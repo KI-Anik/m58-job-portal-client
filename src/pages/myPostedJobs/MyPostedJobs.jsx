626d34e9ea8569319511225f18507a57f2b945c7
KI-Anik <iamkhairul101@gmail.com>
2025-02-16 22:54:57 +0600

    add MyPostedJobs page; implement job fetching and display job details in a table

--- src/pages/myPostedJobs/MyPostedJobs.jsx
+++ src/pages/myPostedJobs/MyPostedJobs.jsx
@@ -1,9 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import UseAuth from '../../hooks/UseAuth';
 
 const MyPostedJobs = () => {
+    const [jobs, setJobs] = useState([])
+    const { user } = UseAuth()
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/jobs?email=${user?.email}`)
+        .then(res => res.json())
+        .then(data => {
+            setJobs(data)
+        })
+    }, [user.email])
+console.log(jobs)
     return (
         <div>
-            
+            <h2>My posted job length: {jobs.length}</h2>
+            <div className="overflow-x-auto">
+            <table className="table">
+                {/* head */}
+                <thead>
+                    <tr>
+                        <th>
+                           
+                        </th>
+                        <th>company Name</th>
+                        <th>Job title</th>
+                        <th>Location</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        jobs.map((job,idx) => <tr key={job._id}>
+                            <th>
+                               <td>{idx+1}</td>
+                            </th>
+                            <td>{job.cpmpany}</td>
+                           <td>{job.title}</td>
+                           <td>{job.location}</td>
+                            <th>
+                                <button className="btn btn-xs">Delete</button>
+                            </th>
+                        </tr>)
+                    }
+                </tbody>
+            </table>
+        </div>
+
         </div>
     );
 };
